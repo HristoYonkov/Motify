f222570853a2e41865abae601dcc42ee2d0dcd46
Hristo Yonkov <hristopturs@gmail.com>
2023-08-29 11:46:15 +0300

    Update Sidebar.tsx

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -12,6 +13,18 @@ const Sidebar: React.FC<SidebarProps> = ({
 }) => {
 
     const pathName = usePathname();
+    const routes = useMemo(() => [
+        {
+            label: 'Home',
+            active: pathName !== '/search',
+            href: '/',
+        },
+        {
+            label: 'Search',
+            active: pathName === '/search',
+            href: '/search',
+        }
+    ], [])
 
     return (
         <div>{children}</div>
