301d77466a33327264b543b69779e466f1112522
Hristo Yonkov <hristopturs@gmail.com>
2023-09-01 19:01:16 +0300

    Update ListItem.tsx

--- client/components/ListItem.tsx
+++ client/components/ListItem.tsx
@@ -23,7 +23,8 @@ const ListItem: React.FC<ListItemProps> = ({
 
 
   return (
-    <button className="
+    <button onClick={onClick}
+      className="
       relative
       group
       flex
@@ -47,7 +48,21 @@ const ListItem: React.FC<ListItemProps> = ({
       <p className="font-medium truncate py-5">
         {name}
       </p>
-      <div className="">
+      <div className="
+        absolute
+        transition
+        opacity-0
+        rounded-full
+        flex
+        items-center
+        justify-center
+        bg-green-500
+        p-4
+        drop-shadow-md
+        right-5
+        group-hover:opacity-100
+        hover:scale-110
+      ">
         <FaPlay className="text-black" />
       </div>
     </button>
