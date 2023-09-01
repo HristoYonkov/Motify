cd00f901648a3b4bc3478cb4a166bb6498d77b12
Hristo Yonkov <hristopturs@gmail.com>
2023-09-01 17:33:12 +0300

    Update Header.tsx

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -5,6 +5,7 @@ import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -31,7 +32,7 @@ const Header: React.FC<HeaderProps> = ({
         `,
             className
         )}>
-            <div className="w-full mb-4= flex items-center justify-between">
+            <div className="w-full mb-4 flex items-center justify-between">
                 <div className="hidden md:flex gap-x-2 items-center">
                     <button
                         onClick={() => router.back()}
@@ -90,7 +91,33 @@ const Header: React.FC<HeaderProps> = ({
                         <BiSearch className='text-black' size={20} />
                     </button>
                 </div>
+                <div className="
+                    flex
+                    justify-between
+                    items-center
+                    gap-x-4
+                ">
+                    <>
+                        <div>
+                            <Button
+                                onClick={() => { }}
+                                className="bg-transperant text-neutral-300 font-medium"
+                            >
+                                Sign up
+                            </Button>
+                        </div>
+                        <div>
+                            <Button
+                                onClick={() => { }}
+                                className="bg-white px-6 py-2"
+                            >
+                                Log in
+                            </Button>
+                        </div>
+                    </>
+                </div>
             </div>
+            {children}
         </div>
     )
 }
