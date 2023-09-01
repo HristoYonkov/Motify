0c97ef57c312fc2c06ad5f44764d8b409a0345f3
Hristo Yonkov <hristopturs@gmail.com>
2023-09-01 17:33:08 +0300

    Update page.tsx

--- client/app/(site)/page.tsx
+++ client/app/(site)/page.tsx
@@ -4,13 +4,14 @@ export default function Home() {
   return (
     <div className="
     bg-neutral-900
-    rounded-lg
-    h-full w-full
-    overflow-hidden
-    overflow-y-auto
+      rounded-lg
+      h-full
+      w-full
+      overflow-hidden
+      overflow-y-auto
     ">
       <Header>
-
+        Header
       </Header>
     </div>
   )
