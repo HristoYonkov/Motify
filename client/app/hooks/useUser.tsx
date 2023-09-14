5d8c75da2564433fd504a346a0df6d5d6dbb63ae
Hristo Yonkov <hristopturs@gmail.com>
2023-09-14 17:51:51 +0300

    Update useUser.tsx

--- client/app/hooks/useUser.tsx
+++ client/app/hooks/useUser.tsx
@@ -1,7 +1,5 @@
-import { createContext, useEffect, useState } from "react";
-import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react";
-import { Subscription } from "@supabase/gotrue-js";
-import { User } from "@supabase/auth-helpers-nextjs";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useSessionContext, useUser as useSupaUser, User } from "@supabase/auth-helpers-react";
 
 import { ISubscription, IUserDetails } from "@/types";
 
@@ -77,4 +75,13 @@ export const MyUserContextProvider = (props: IProps) => {
     };
 
     return <UserContext.Provider value={value} {...props} />
+}
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+        throw new Error('useUser must be used within a MyUserContextProvider!!')
+    }
+
+    return context;
 }
