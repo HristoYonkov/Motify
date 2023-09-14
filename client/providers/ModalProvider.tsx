5178f8241f14c95dc757ab897f01733c10efaa1d
Hristo Yonkov <hristopturs@gmail.com>
2023-09-14 18:13:17 +0300

    Create ModalProvider.tsx

---
+++ client/providers/ModalProvider.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useState } from "react";
+
+
+const ModalProvider = () => {
+    const [mounted, setMounted] = useState(false);
+  return (
+    <>
+        Modals!
+    </>
+  )
+}
+
+export default ModalProvider
