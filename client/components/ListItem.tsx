35034dbcadcc337e4c3e20bdbd10f3eb88cda35c
Hristo Yonkov <hristopturs@gmail.com>
2023-09-17 14:19:35 +0300

    Update ListItem.tsx

--- client/components/ListItem.tsx
+++ client/components/ListItem.tsx
@@ -25,16 +25,16 @@ const ListItem: React.FC<ListItemProps> = ({
   return (
     <button onClick={onClick}
       className="
-      relative
-      group
-      flex
-      items-center
-      rounded-md
-      overflow-hidden
-      gap-x-4
+      relative 
+      group 
+      flex 
+      items-center 
+      rounded-md 
+      overflow-hidden 
+      gap-x-4 
       bg-neutral-100/10
       hover:bg-neutral-100/20
-      transition
+      transition 
       pr-4
     ">
       <div className="relative min-h-[64px] min-w-[64px]">
