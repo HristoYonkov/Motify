15e3b707ed275557e0633b23a74fc995a8a7e216
Hristo Yonkov <hristopturs@gmail.com>
2023-09-20 15:14:28 +0300

    Update AuthModal.tsx

--- client/components/AuthModal.tsx
+++ client/components/AuthModal.tsx
@@ -6,17 +6,26 @@ import { useRouter } from "next/navigation";
 import Modal from "./Modal";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+import useAuthModal from "@/hooks/useAuthModal";
 
 const AuthModal = () => {
     const supabaseClient = useSupabaseClient();
     const router = useRouter();
     const { session } = useSessionContext();
+    const {onClose, isOpen} = useAuthModal();
+
+    const onChange =(open: boolean) => {
+        if (!open) {
+            onClose();
+        }
+    }
+
 
     return (
         <Modal
             title="Welcome Back"
             description="Log in to your account"
-            isOpen
+            isOpen={isOpen}
             onChange={() => { }}
         >
             <Auth
