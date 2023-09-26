0a5c40a800f6617696d93c45a05ba9f4792f623a
Hristo Yonkov <hristopturs@gmail.com>
2023-09-26 17:32:37 +0300

    Update UploadModal.tsx

--- client/components/UploadModal.tsx
+++ client/components/UploadModal.tsx
@@ -1,13 +1,36 @@
+'use client';
 
+import { useForm } from 'react-hook-form';
+
+import useUploadModal from '@/hooks/useUploadModal';
 import Modal from './Modal';
 
 const UploadModal = () => {
+    const uploadModal = useUploadModal();
+
+    const {register, handleSubmit, reset} = useForm({
+        defaultValues: {
+            author: '',
+            title: '',
+            song: null,
+            image: null,
+        }
+    });
+
+    const onChange = (open: boolean) => {
+        if (!open) {
+            reset();
+            uploadModal.onClose();
+        }
+    }
+
+
     return (
         <Modal
-            title='Upload modal title'
-            description='Upload modal description'
-            isOpen
-            onChange={() => { }}
+            title='Add a song'
+            description='Upload an .mp3 file.'
+            isOpen={uploadModal.isOpen}
+            onChange={onChange}
         >
             Upload Content!
         </Modal>
