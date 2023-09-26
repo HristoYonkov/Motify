36f295fd41ddff60fba8db6cb00e1667d7ade992
Hristo Yonkov <hristopturs@gmail.com>
2023-09-26 16:38:28 +0300

    Create UploadModal.tsx

---
+++ client/components/UploadModal.tsx
@@ -0,0 +1,17 @@
+
+import Modal from './Modal';
+
+const UploadModal = () => {
+    return (
+        <Modal
+            title='Upload modal title'
+            description='Upload modal description'
+            isOpen
+            onChange={() => { }}
+        >
+            Upload Content!
+        </Modal>
+    )
+}
+
+export default UploadModal;
