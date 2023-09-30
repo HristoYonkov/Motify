5b960268408357592ef8a6327678563db376deea
Hristo Yonkov <hristopturs@gmail.com>
2023-09-30 14:21:50 +0300

    Update SongItem.tsx

--- client/components/SongItem.tsx
+++ client/components/SongItem.tsx
@@ -47,8 +47,21 @@ const SongItem: React.FC<SongItemProps> = ({
                     className="object-cover"
                     src={imagePath || '/images/liked.png'}
                     fill
+                    sizes="fill"
+                    priority={false}
                     alt="Image"
                 />
+            </div>
+            <div className="flex flex-col items-start w-full pt-4 gap-y-1">
+                <p className="font-semibold truncate w-full">
+                    {data.title}
+                </p>
+                <p className="text-neutral-400 text-sm pb-4 w-full truncate">
+                    By {data.author}
+                </p>
+            </div>
+            <div className="absolute bottom-24 right-5">
+                Play Button
             </div>
         </div>
     )
