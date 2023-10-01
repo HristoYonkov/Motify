b1a3f2416c64a57550aff5adb39f075b0d74763a
Hristo Yonkov <hristopturs@gmail.com>
2023-10-01 13:05:51 +0300

    Create MediaItem.tsx

---
+++ client/components/MediaItem.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import Image from "next/image";
+
+import useLoadImage from "@/hooks/useLoadImage";
+import { ISong } from "@/types";
+
+interface MediaItemProps {
+    data: ISong;
+    onClick?: (id: string) => void;
+}
+
+const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
+    const imageUrl = useLoadImage(data);
+
+    const handleClick = () => {
+        if (onClick) {
+            return onClick(data.id)
+        }
+
+        // TODO: Default turn on player!
+    }
+
+
+    return (
+        <div className="
+            flex
+            items-center
+            gap-x-3
+            cursor-pointer
+            hover:bg-neutral-800/50
+            w-full
+            p-2
+            rounded-md
+        "
+            onClick={handleClick}
+        >
+            <div className="
+                relative
+                rounded-md
+                min-h-[48px]
+                min-w-[48px]
+                overflow-hidden
+            ">
+                <Image
+                    fill
+                    src={imageUrl || '/images/liked.png'}
+                    alt="Media Item"
+                    className="object-cover"
+                    sizes="fill"
+                    priority={false}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default MediaItem;
