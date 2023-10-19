11d76aec5f793a3f6c52718d1f982f479e0f4bf9
Hristo Yonkov <hristopturs@gmail.com>
2023-10-19 14:07:43 +0300

    Update LikedContent.tsx

--- client/app/liked/components/LikedContent.tsx
+++ client/app/liked/components/LikedContent.tsx
@@ -1,9 +1,20 @@
-'use client'
+'use client';
 
-const LikedContent = () => {
-  return (
-    <div>LikedContent</div>
-  );
+import { useUser } from "@/hooks/useUser";
+import { ISong } from "@/types";
+import { useRouter } from "next/navigation";
+
+interface LikedContentProps {
+    songs: ISong[];
+}
+
+const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
+    const router = useRouter();
+    const { user, isLoading } = useUser();
+
+    return (
+        <div>LikedContent</div>
+    );
 }
 
 export default LikedContent;
