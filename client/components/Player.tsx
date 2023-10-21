1fdc11a2480e5d9bf4c0e08efa3f027a5fad84b7
Hristo Yonkov <hristopturs@gmail.com>
2023-10-21 17:00:30 +0300

    Update Player.tsx

--- client/components/Player.tsx
+++ client/components/Player.tsx
@@ -1,10 +1,19 @@
 'use client';
 
+import useGetSongById from "@/hooks/useGetSongById";
+import usePlayer from "@/hooks/usePlayer";
+
 
 const Player = () => {
-  return (
-    <div>Player</div>
-  )
+    const player = usePlayer();
+    const { song } = useGetSongById();
+
+
+    return (
+        <div>
+            Player
+        </div>
+    )
 }
 
 export default Player;
