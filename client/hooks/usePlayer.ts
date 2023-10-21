480035478a5ba21de043877e116907d10c381f10
Hristo Yonkov <hristopturs@gmail.com>
2023-10-21 15:26:08 +0300

    Update usePlayer.ts

--- client/hooks/usePlayer.ts
+++ client/hooks/usePlayer.ts
@@ -1,7 +1,19 @@
+import { create } from "zustand";
+
 interface PlayerStore {
-    id: string[];
-    activated?: string;
+    ids: string[];
+    activeId?: string;
     setId: (id: string) => void;
     setIds: (ids: string[]) => void;
     reset: () => void;
 }
+
+const usePlayer = create<PlayerStore>((set) => ({
+    ids: [],
+    activeId: undefined,
+    setId: (id: string) => set({ activeId: id }),
+    setIds: (ids: string[]) => set({ ids: ids }),
+    reset: () => set({ ids: [], activeId: undefined })
+}));
+
+export default usePlayer;
