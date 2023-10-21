e9a0b9a79ff4f6273060d72ccd6de3869a074b4f
Hristo Yonkov <hristopturs@gmail.com>
2023-10-21 16:03:51 +0300

    Create useGetSongById.ts

---
+++ client/hooks/useGetSongById.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import { useSessionContext } from '@supabase/auth-helpers-react';
+import toast from 'react-hot-toast';
+
+import { ISong } from '@/types';
+
+const useGetSongById = (id?: string) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [song, setSong] = useState<ISong | undefined>(undefined);
+    const { supabaseClient } = useSessionContext();
+
+    useEffect(() => {
+        if (!id) {
+            return;
+        }
+
+        setIsLoading(true);
+
+        const fetchSong = async () => {
+            const { data, error } = await supabaseClient
+            .from('songs')
+            .select('*')
+            .eq('id', id)
+            .single();
+
+            if (error) {
+                setIsLoading(false);
+                return toast.error(error.message);
+            }
+
+            setSong(data as ISong);
+            setIsLoading(false);
+        }
+
+        fetchSong();
+    }, [id, supabaseClient])
+}
+
+export default useGetSongById
