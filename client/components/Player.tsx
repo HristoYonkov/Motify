52ff467f9279d931e505549fe5a3d0b79e8824e7
Hristo Yonkov <hristopturs@gmail.com>
2023-10-22 15:43:26 +0300

    Update Player.tsx

--- client/components/Player.tsx
+++ client/components/Player.tsx
@@ -10,8 +10,20 @@ const Player = () => {
     const { song } = useGetSongById(player.activeId);
     const songUrl = useLoadSongUrl(song!);
 
+    if (!song || !songUrl || !player.activeId) {
+        return null;
+    }
+
     return (
-        <div>
+        <div className="
+            fixe
+            bottom-0
+            bg-black
+            w-full
+            py-2
+            h-[80px]
+            px-4
+        ">
             Player
         </div>
     )
