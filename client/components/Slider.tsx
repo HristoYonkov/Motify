284d7b1b8151162aa214d0ceb1addd622bee25fd
Hristo Yonkov <hristopturs@gmail.com>
2023-10-26 15:20:16 +0300

    Create Slider.tsx

---
+++ client/components/Slider.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import * as RadixSlider from '@radix-ui/react-slider';
+
+interface SliderProps {
+    value?: number;
+    onChange?: (value: number) => void;
+}
+
+const Slider: React.FC<SliderProps> = ({ value = 1, onChange }) => {
+    
+
+
+    return (
+        <div>Slider</div>
+    )
+}
+
+export default Slider
