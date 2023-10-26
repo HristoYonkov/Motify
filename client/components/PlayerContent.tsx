f9e60f16428ab2a15a84c48e9bbb66f8dc1c938e
Hristo Yonkov <hristopturs@gmail.com>
2023-10-26 16:06:16 +0300

    Update PlayerContent.tsx

--- client/components/PlayerContent.tsx
+++ client/components/PlayerContent.tsx
@@ -4,6 +4,7 @@ import { BsPauseFill, BsPlayFill } from 'react-icons/bs'
 import { AiFillStepBackward, AiFillStepForward } from 'react-icons/ai';
 import { HiSpeakerXMark, HiSpeakerWave } from 'react-icons/hi2';
 import { useState } from 'react';
+import useSound from 'use-sound';
 
 import { ISong } from "@/types";
 
@@ -34,7 +35,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
         const currentIndex = player.ids.findIndex((id) => id === player.activeId);
         const nextSong = player.ids[currentIndex + 1];
 
-        if(!nextSong) {
+        if (!nextSong) {
             return player.setId(player.ids[0]);
         }
 
@@ -49,12 +50,26 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
         const currentIndex = player.ids.findIndex((id) => id === player.activeId);
         const prevSong = player.ids[currentIndex - 1];
 
-        if(!prevSong) {
+        if (!prevSong) {
             return player.setId(player.ids[player.ids.length - 1]);
         }
 
         player.setId(prevSong);
     }
+
+    const [play, { pasue, sound }] = useSound(
+        songUrl,
+        {
+            volume: volume,
+            onplay: () => setIsPlaying(true),
+            onend: () => {
+                setIsPlaying(false);
+                onPlayNext();
+            },
+            onpause: () => setIsPlaying(false),
+            format: ['mp3']
+        }
+    );
 
     return (
         <div className="grid grid-cols-2 md:grid-cols-3 h-full">
