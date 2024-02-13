a252a4c3d025c2524cfecfdce75edc1f25b9dd04
Hristo Yonkov <hristopturs@gmail.com>
2024-02-13 18:12:54 +0200

    Update getSongsByUserId.ts

--- client/actions/getSongsByUserId.ts
+++ client/actions/getSongsByUserId.ts
@@ -1,32 +1,31 @@
-import { ISong } from "@/types";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies, headers } from "next/headers";
+import { cookies } from "next/headers";
+
+import { ISong } from "@/types";
 
 const getSongsByUserId = async (): Promise<ISong[]> => {
-    const supabase = createServerComponentClient({
-        cookies: cookies
-    });
+  const supabase = createServerComponentClient({
+    cookies: cookies
+  });
 
-    const { data: sessionData,
-        error: sessionError
-    } = await supabase.auth.getSession();
+  const { data: sessionData, error: sessionError } = await supabase.auth.getSession();
 
-    if (sessionError) {
-        console.log(sessionError.message);
-        return [];
-    }
+  if (sessionError) {
+    console.log(sessionError.message);
+    return [];
+  }
 
-    const { data, error } = await supabase
-        .from('songs')
-        .select('*')
-        .eq('user_id', sessionData.session?.user.id)
-        .order('created_at', { ascending: false });
+  const { data, error } = await supabase
+    .from('songs')
+    .select('*')
+    .eq('user_id', sessionData.session?.user.id)
+    .order('created_at', { ascending: false })
 
-    if (error) {
-        console.log(error.message);
-    }
+  if (error) {
+    console.log(error.message);
+  }
 
-    return (data as any) || [];
+  return (data as any) || [];
 };
 
 export default getSongsByUserId;
