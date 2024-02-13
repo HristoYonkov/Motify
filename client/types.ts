8dec3db3fda7abfbc9bed82e6f046fb49791837f
Hristo Yonkov <hristopturs@gmail.com>
2024-02-13 17:06:55 +0200

    Update types.ts

--- client/types.ts
+++ client/types.ts
@@ -1,72 +1,72 @@
-import Stripe from "stripe";
+import Stripe from 'stripe';
 
 export interface ISong {
-    id: string;
-    user_id: string;
-    author: string;
-    title: string;
-    song_path: string;
-    image_path: string;
+  id: string;
+  user_id: string;
+  author: string;
+  title: string;
+  song_path: string;
+  image_path: string;
 }
 
 export interface IProduct {
-    id: string;
-    active?: boolean;
-    name?: string;
-    description?: string;
-    image?: string;
-    metadata?: Stripe.Metadata;
+  id: string;
+  active?: boolean;
+  name?: string;
+  description?: string;
+  image?: string;
+  metadata?: Stripe.Metadata;
 }
 
 export interface IPrice {
-    id: string;
-    product_id?: string;
-    active?: boolean;
-    description?: string;
-    unit_amount?: number;
-    currency?: string;
-    type?: Stripe.Price.Type;
-    interval?: Stripe.Price.Recurring.Interval;
-    interval_count?: number;
-    trial_period_days?: number | null;
-    metadata?: Stripe.Metadata;
-    product?: IProduct;
+  id: string;
+  product_id?: string;
+  active?: boolean;
+  description?: string;
+  unit_amount?: number;
+  currency?: string;
+  type?: Stripe.Price.Type;
+  interval?: Stripe.Price.Recurring.Interval;
+  interval_count?: number;
+  trial_period_days?: number | null;
+  metadata?: Stripe.Metadata;
+  products?: IProduct;
 }
 
 export interface ICustomer {
-    id: string;
-    stripe_customer_id?: string;
+  id: string;
+  stripe_customer_id?: string;
 }
 
 export interface IUserDetails {
-    id: string;
-    first_name: string;
-    last_name: string;
-    full_name?: string;
-    avatar_url?: string;
-    billing_address?: Stripe.Address;
-    payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type]
+  id: string;
+  first_name: string;
+  last_name: string;
+  full_name?: string;
+  avatar_url?: string;
+  billing_address?: Stripe.Address;
+  payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
 }
 
 export interface IProductWithPrice extends IProduct {
-    prices?: IPrice[];
+  prices?: IPrice[];
 }
 
 export interface ISubscription {
-    id: string;
-    user_id: string;
-    status?: Stripe.Subscription.Status;
-    metadata?: Stripe.Metadata;
-    price_id?: string;
-    quantity?: number;
-    cancel_at_period_end?: boolean;
-    created: string;
-    current_period_start: string;
-    current_period_end: string;
-    ended_at?: string;
-    cancel_at?: string;
-    canceled_at?: string;
-    trial_start?: string;
-    trial_end?: string;
-    prices?: IPrice;
+  id: string;
+  user_id: string;
+  status?: Stripe.Subscription.Status;
+  metadata?: Stripe.Metadata;
+  price_id?: string;
+  quantity?: number;
+  cancel_at_period_end?: boolean;
+  created: string;
+  current_period_start: string;
+  current_period_end: string;
+  ended_at?: string;
+  cancel_at?: string;
+  canceled_at?: string;
+  trial_start?: string;
+  trial_end?: string;
+  prices?: IPrice;
 }
